4ef3a86cd7795ac353f1673ba5a49a0fa1043a0f
Ivan Kashin <barbos1701@gmail.com>
2020-06-01 20:16:17 +0300

    Выполняет задание, делает колонки цветными, по вертикали

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,20 @@
 'use strict';
 
-var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 200;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
-var CLOUD_Y = 50;
-var GAP = 10;
+var CLOUD_Y = 10;
+
+var GAP_X = 50;
+var GAP_Y = 10;
+
 var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var BAR_WIDTH = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var GIST_START_Y = CLOUD_Y + (GAP_Y + FONT_GAP) * 2 + GAP_Y;
+
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
+
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -28,15 +34,28 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X + GAP_Y, CLOUD_Y + GAP_Y, 'rgba(0, 0, 0, 0.3)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.fillStyle = '#000';
 
   var maxTime = getMaxElement(times);
 
+  ctx.font = '16px PT Mono';
+
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP_Y, CLOUD_Y + GAP_Y + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP_Y, CLOUD_Y + (GAP_Y + FONT_GAP) * 2);
+
   for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (BAR_WIDTH * times[i]) / maxTime, BAR_HEIGHT);
+    var currentHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    var gistStartOffsetY = BAR_HEIGHT - currentHeight;
+
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_X + (BAR_WIDTH + GAP_X) * i, GIST_START_Y + gistStartOffsetY + FONT_GAP);
+    ctx.fillText(players[i], CLOUD_X + GAP_X + (BAR_WIDTH + GAP_X) * i, GIST_START_Y + FONT_GAP + GAP_Y + BAR_HEIGHT + GAP_Y + FONT_GAP);
+
+    ctx.fillStyle = players[i] === 'Вы' ? PLAYER_COLOR : 'hsl(220, ' + Math.round(Math.random() * 100) + '%, 50%)';
+    ctx.fillRect(CLOUD_X + GAP_X + (BAR_WIDTH + GAP_X) * i, GIST_START_Y + FONT_GAP + GAP_Y + gistStartOffsetY, BAR_WIDTH, currentHeight);
+
+    ctx.fillStyle = '#000';
   }
 };
