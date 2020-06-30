6487b225cc47c2921312bd5d0ea8a724c8bde78c
Ivan Kashin <barbos1701@gmail.com>
2020-06-30 20:43:00 +0300

    Рефактор бэкенда и добавление save

--- js/backend.js
+++ js/backend.js
@@ -1,39 +1,68 @@
 'use strict';
 
 window.backend = (function () {
-  var URL = 'https://javascript.pages.academy/code-and-magick/data';
+  var URL = {
+    'GET': 'https://javascript.pages.academy/code-and-magick/data',
+    'POST': 'https://javascript.pages.academy/code-and-magick'
+  };
+
   var STATUS_CODES = {
     OK: 200
   };
   var TIMEOUT_IN_MS = 10000;
 
-  var load = function (onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
+  var addOnRequestLoad = function (xhr, onLoad, onError) {
     xhr.addEventListener('load', function () {
       if (xhr.status === STATUS_CODES.OK) {
-        onSuccess(xhr.response);
+        onLoad(xhr.response);
       } else {
         onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
+  };
 
+  var addOnRequestError = function (xhr, onError) {
     xhr.addEventListener('error', function () {
       onError('Произошла ошибка соединения!');
     });
+  };
 
+  var addOnRequestTimeout = function (xhr, onError) {
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + TIMEOUT_IN_MS + 'мс');
     });
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    addOnRequestLoad(xhr, onLoad, onError);
+    addOnRequestError(xhr, onError);
+    addOnRequestTimeout(xhr, onError);
 
     xhr.timeout = TIMEOUT_IN_MS;
 
-    xhr.open('GET', URL);
+    xhr.open('GET', URL.GET);
     xhr.send();
   };
 
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    addOnRequestLoad(xhr, onLoad, onError);
+    addOnRequestError(xhr, onError);
+    addOnRequestTimeout(xhr, onError);
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('POST', URL.POST);
+    xhr.send(data);
+  };
+
   return {
-    load: load
+    load: load,
+    save: save
   };
 })();
